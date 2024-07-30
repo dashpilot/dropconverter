363c5b009e138a9977c17ba6c4472eb4db7dd518
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2024-07-30 13:17:47 +0200

    added oauth route

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from 'svelte';
+
+  
+    onMount(() => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const token = urlParams.get('token');
+      if (token) {
+        console.log(token);
+        localStorage.setItem('token', token);
+      }
+    });
+  </script>
